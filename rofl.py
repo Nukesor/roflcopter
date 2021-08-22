39399504286bfaf4bb60af665b68ee75ef82917d
Arne Beer <contact@arne.beer>
2021-08-22 18:54:06 +0200

    Refactor

--- rofl.py
+++ rofl.py
@@ -4,43 +4,62 @@ import math
 import os
 import random
 
-terminal_width = os.get_terminal_size()
-thingy = "ROFLCOPTER"
-length = len(thingy)
+
+class App:
+    def __init__(self):
+        """Initialize the app and the initial screen."""
+        self.thingy = "ROFLCOPTER"
+        self.terminal_width = os.get_terminal_size()
+        self.sleep_factor = random.randrange(1, 5)
+
+        # Initialize the screen
+        self.offset = 0
+        for _ in range(0, self.terminal_width.lines):
+            self.print_line()
+            self.increment_offset()
+
+    def increment_offset(self):
+        self.offset += 1
+        self.offset = self.offset % len(self.thingy)
+
+    def calculate_sleep(self, negative: bool):
+        """Calculate the current sleep time and whether a new random."""
+        sleep_time = math.sin(time.time())
+
+        # Whenever we switch from positive to negative in the sinus curve,
+        # we assign a new random sleep_factor is assigned.
+        if sleep_time > 0 and negative:
+            negative = False
+            self.sleep_factor = random.randrange(1, 5) / 10
+        elif sleep_time < 0 and not negative:
+            negative = True
+            self.sleep_factor = random.randrange(1, 5) / 10
+
+        sleep_time = abs(sleep_time) * self.sleep_factor
+
+        return sleep_time
+
+    def print_line(self):
+        """Print the thingy"""
+        first = self.thingy[0 : self.offset]
+        second = self.thingy[self.offset : len(self.thingy)]
+        string = f"{second}{first} "
+        full_string = string * int(self.terminal_width.columns / (len(self.thingy) + 1))
+        print(full_string)
+
+    def run(self):
+        """The main update loop of this program."""
+        negative = True
+        while True:
+            self.print_line()
+            self.increment_offset()
+            sleep_time = self.calculate_sleep(negative)
+            time.sleep(sleep_time)
 
 
 def main():
-    offset = 0
-    for _ in range(0, terminal_width.lines):
-        print_line(offset)
-        offset += 1
-        offset = offset % length
-
-    negative = True
-    sleep_factor = random.randrange(1, 5)
-    while True:
-        print_line(offset)
-        offset += 1
-        offset = offset % length
-        sleep_time = math.sin(time.time())
-
-        if sleep_time > 0 and negative:
-            negative = False
-            sleep_factor = random.randrange(1, 5)
-        elif sleep_time < 0 and not negative:
-            negative = True
-            sleep_factor = random.randrange(1, 5)
-
-        time.sleep(abs(sleep_time) * sleep_factor / 10)
-
-
-def print_line(offset):
-    """Print the thingy"""
-    first = thingy[0:offset]
-    second = thingy[offset:length]
-    string = f"{second}{first}"
-    full_string = string * int(terminal_width.columns / length)
-    print(full_string)
+    app = App()
+    app.run()
 
 
 main()
