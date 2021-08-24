2ddccae228b61493f11cb9952b4c83bb6f1cba52
Arne Beer <contact@arne.beer>
2021-08-24 19:59:12 +0200

    fix: Fix colors

--- src/color.rs
+++ src/color.rs
@@ -11,13 +11,13 @@ pub fn create_colors() -> Vec<Color> {
         // Bright green
         Color::from_rgba(153, 255, 0, 255),
         // Green
-        Color::from_rgba(35, 255, 0, 255),
+        Color::from_rgba(38, 255, 0, 255),
         // Turquoise
-        Color::from_rgba(255, 198, 0, 255),
+        Color::from_rgba(255, 200, 0, 255),
         // Bright blue
-        Color::from_rgba(0, 194, 255, 255),
+        Color::from_rgba(0, 195, 255, 255),
         // Blue
-        Color::from_rgba(0, 75, 255, 255),
+        Color::from_rgba(0, 76, 255, 255),
         //// Dark Blue
         //Color::from_rgba(42, 0, 255, 255),
         // Purple
